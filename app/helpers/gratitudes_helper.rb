8546ddd9fadf2df2f30c4f477239ca67ebbbac96
Brian Cheung <bdcheung@gmail.com>
2013-11-01 15:44:18 -0400

    Cleaned up some view bugs, refined list of common english words

--- app/helpers/gratitudes_helper.rb
+++ app/helpers/gratitudes_helper.rb
@@ -1,4 +1,3 @@
 module GratitudesHelper
-	COMMON_ENGLISH_WORDS = [ "the", "a", "an"
-	]
+	COMMON_ENGLISH_WORDS = ["is", "am", "are", "was", "were", "be", "being", "been", "do", "did", "does", "has", "have", "had", "will", "shall", "and", "or", "in", "on", "a", "to", "for", "as"]
 end
